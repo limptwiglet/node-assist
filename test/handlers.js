86bbac3b1232c35509552fde9c4f02c50b4cd32d
Mark Gerrard <limptwiglet@gmail.com>
2012-09-06 17:47:23 +0100

    Updated file system handler to throw an error when fs.exists fails

--- test/handlers.js
+++ test/handlers.js
@@ -1,28 +1,30 @@
 var expect = require('chai').expect
-	, assist = require('../');
+    , assist = require('../');
 
 
 describe('FileSystemHandler', function(){
-	var a1;
-	var handler;
-	before(function(done){
-		handler = assist.handlers.FileSystem;
-		a1 = assist.Assistant;
-		done();
-	});
-	describe('find', function(){
-		it('should set parameters', function(){
-			var fsh = new handler('/some/path', {test: 'value'});
-			expect(fsh).to.have.property('basePath');
-		});
-		it('should fail to find file', function(){
-			a1.configure({
-				handler: new handler('/some/path', {test: 'value'})
-			});
-			a1.find('', function(err, file){
-				console.log('here');
-				console.log(err, file);
-			});
-		});
-	});
+    var a1;
+    var handler;
+    before(function(done){
+        handler = assist.handlers.FileSystem;
+        a1 = assist.Assistant;
+        done();
+    });
+    describe('find', function(){
+        it('should set parameters', function(){
+            var fsh = new handler('/some/path', {test: 'value'});
+            expect(fsh).to.have.property('basePath');
+        });
+
+        it('should fail to find file', function(done){
+            a1.configure({
+                handler: new handler('/some/path', {test: 'value'})
+            });
+
+            a1.find('', function(err, file){
+                expect(err).to.be.instanceof(Error);
+                done();
+            });
+        });
+    });
 });
